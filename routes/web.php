d982688ad3c6a9995f0dca465a9c487c3476c3d7
Egor Grishin <egor-grishin@bk.ru>
2023-09-04 22:08:37 +0500

    bot route

--- routes/web.php
+++ routes/web.php
@@ -13,14 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::any('/bot', function (\Illuminate\Http\Request $request) {
-    \Illuminate\Support\Facades\Log::debug([
-        $request->method(),
-        $request->all()
-    ]);
-});
-
-Route::get('/', function () {
+Route::get('/333', function () {
     $response = \Telegram\Bot\Laravel\Facades\Telegram::bot()->sendMessage([
         'chat_id' => '935824965',
         'text' => 'Hello World'
@@ -30,3 +23,10 @@ Route::get('/', function () {
     dd($messageId, $response);
     return view('welcome');
 });
+
+Route::get('/{any}', function (\Illuminate\Http\Request $request) {
+    \Illuminate\Support\Facades\Log::debug([
+        $request->method(),
+        $request->all()
+    ]);
+})->where('any', '.*');
