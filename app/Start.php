311627cd171fba078a881f7fe9c3fdad6468f005
Egor Grishin <egor-grishin@bk.ru>
2023-09-05 01:32:43 +0500

    logging

--- app/Start.php
+++ app/Start.php
@@ -3,12 +3,15 @@
 namespace App;
 
 use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Log;
 
 class Start
 {
-    public function start(): JsonResponse
+    public function start(Request $request): JsonResponse
     {
+        Log::debug($request->all());
         $url = 'https://api.telegram.org/';
         $bot = 'bot6521726004:AAHh86wPhEu2tg_DJethX90BxmOq4BUw5ks/';
         Http::post($url . $bot . 'sendMessage', [
