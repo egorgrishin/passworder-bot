a630bbfa176990edb8a314e87b098f98d7a6cfff
Egor Grishin <egor-grishin@bk.ru>
2023-09-13 23:13:05 +0500

    remove stages table

--- database/migrations/2023_09_05_193004_create_chats_table.php
+++ database/migrations/2023_09_05_193004_create_chats_table.php
@@ -15,13 +15,7 @@ return new class extends Migration
             $table->uuid()->primary();
             $table->string('hash', 60)->unique();
             $table->string('password')->nullable();
-            $table->tinyInteger('stage_id')->nullable()->unsigned()->index();
-
-            $table->foreign('stage_id')
-                ->references('id')
-                ->on('stages')
-                ->nullOnDelete()
-                ->cascadeOnUpdate();
+            $table->string('stage', 63)->nullable();
         });
     }
 
