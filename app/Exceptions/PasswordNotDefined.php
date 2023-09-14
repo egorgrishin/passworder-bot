b5e928127786ae1bcf3efee571a286e2fb42067c
Egor Grishin <egor-grishin@bk.ru>
2023-09-14 22:27:40 +0500

    PasswordDefined middleware

---
+++ app/Exceptions/PasswordNotDefined.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Exceptions;
+
+use App\Contracts\TelegramException as TelegramInterface;
+use App\Helpers\Telegram\Telegram;
+use Illuminate\Support\Facades\DB;
+
+class PasswordNotDefined extends TelegramException implements TelegramInterface
+{
+    private string $hash;
+
+    public function __construct(int $chat_id, string $hash)
+    {
+        parent::__construct($chat_id);
+        $this->hash = $hash;
+    }
+
+    /**
+     * Отправляет сообщение о том что нужно установить пароль
+     */
+    public function sendMessage(): void
+    {
+        $this->updateChatStage();
+        Telegram::send($this->chat_id, 'Пароль не установлен. Введите пароль');
+    }
+
+    /**
+     * Переносит пользователя на этап установки пароля
+     */
+    private function updateChatStage(): void
+    {
+        DB::table('chats')
+            ->where('hash', $this->hash)
+            ->update(['stage' => 'set_password']);
+    }
+}
--- app/Middleware/PasswordDefined.php
+++ app/Middleware/PasswordDefined.php
@@ -2,16 +2,38 @@
 
 namespace App\Middleware;
 
+use App\Exceptions\PasswordNotDefined;
 use Closure;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class PasswordDefined
 {
     /**
      * Handle an incoming request.
+     * @throws PasswordNotDefined
      */
     public function handle(Request $request, Closure $next): mixed
     {
+        $chat = $this->getChatByHash(
+            $hash = $request->input('hash')
+        );
+
+        if ($chat->password === null) {
+            $chat_id = $request->input('message.chat.id');
+            throw new PasswordNotDefined($chat_id, $hash);
+        }
+
         return $next($request);
+    }
+
+    /**
+     * Возвращает чат по хэшу
+     */
+    private function getChatByHash(string $hash): object
+    {
+        return DB::table('chats')
+            ->where('hash', $hash)
+            ->first();
     }
 }
