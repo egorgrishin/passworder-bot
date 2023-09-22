097144619fe531509c7b3f9f084b8cf5bdf43cb8
Egor Grishin <egor-grishin@bk.ru>
2023-09-22 16:59:55 +0500

    log

--- app/Middleware/HashChatId.php
+++ app/Middleware/HashChatId.php
@@ -5,6 +5,7 @@ namespace App\Middleware;
 use App\Helpers\Hasher;
 use Closure;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 
 class HashChatId
 {
@@ -13,6 +14,7 @@ class HashChatId
      */
     public function handle(Request $request, Closure $next): mixed
     {
+        Log::debug($request->all());
         $chat_id = $request->input('message.chat.id');
         $request->offsetSet('hash', Hasher::make($chat_id));
 
